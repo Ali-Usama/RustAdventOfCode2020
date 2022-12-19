6d106df9e23338d23cc024529ebc887cb464d540
Usama Ali <aliusama481@yahoo.com>
2022-12-19 13:03:43 +0500

    [UPDATE] day2 part 1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,6 @@
 use std::ops::RangeInclusive;
 
+#[derive(PartialEq, Debug)]
 struct PasswordPolicy {
     byte: u8,
     range: RangeInclusive<usize>,
@@ -17,7 +18,51 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn parse_line(s: &str) -> anyhow::Result<(PasswordPolicy, &str)> {
-    todo!()
+    let (policy, password) = {
+        let mut tokens = s.split(':');
+        (
+            tokens.next()
+                .ok_or(ParseError::Expected("password policy"))?,
+            tokens.next()
+                .ok_or(ParseError::Expected("password"))?
+                .trim(),
+        )
+    };
+
+    let (range, byte) = {
+        let mut tokens = policy.split(' ');
+        (
+            tokens.next()
+                .ok_or(ParseError::Expected("policy range"))?,
+            tokens.next()
+                .ok_or(ParseError::Expected("policy byte"))?,
+        )
+    };
+
+    let byte = if byte.as_bytes().len() == 1 {
+        byte.as_bytes()[0]
+    } else {
+        return Err(ParseError::Expected("password policy byte to be exactly 1 byte").into());
+    };
+
+    let (min, max) = {
+        let mut tokens = range.split('-');
+        (
+            tokens.next()
+                .ok_or(ParseError::Expected("policy range (lower bound)"))?,
+            tokens.next()
+                .ok_or(ParseError::Expected("policy range (upper bound)"))?
+        )
+    };
+
+    let range = (min.parse()?)..=(max.parse()?);
+
+    println!("{}: {}, {}", policy, min, max);
+
+    Ok((PasswordPolicy {
+        range,
+        byte,
+    }, password))
 }
 
 impl PasswordPolicy {
@@ -32,11 +77,16 @@ impl PasswordPolicy {
     }
 }
 
+#[derive(thiserror::Error, Debug)]
+enum ParseError {
+    #[error("expected {0}")]
+    Expected(&'static str),
+}
 
 
 #[cfg(test)]
 mod tests {
-    use super::PasswordPolicy;
+    use super::{PasswordPolicy, parse_line};
 
     #[test]
     fn test_is_valid() {
@@ -45,9 +95,31 @@ mod tests {
             byte: b'a',
         };
         assert_eq!(pp.is_valid("zeus"), false, "no 'a's");
-        assert_eq!(pp.is_valid("hades"), false, "single 'a's");
-        assert_eq!(pp.is_valid("banana"), false, "three 'a's");
+        assert_eq!(pp.is_valid("hades"), true, "single 'a's");
+        assert_eq!(pp.is_valid("banana"), true, "three 'a's");
         assert_eq!(pp.is_valid("aaaah"), false, "too many 'a's");
+    }
 
+    #[test]
+    fn test_parse() {
+        assert_eq!(
+            parse_line("1-3 a: banana").unwrap(),
+            (
+                PasswordPolicy {
+                    range: 1..=3,
+                    byte: b'a',
+                }, "banana"
+            )
+        );
+
+        assert_eq!(
+            parse_line("1-3 a").unwrap_err().to_string(),
+            "expected password"
+        );
+
+        assert_eq!(
+            parse_line("1-3 : banana").unwrap_err().to_string(),
+            "expected password policy byte to be exactly 1 byte"
+        );
     }
 }
